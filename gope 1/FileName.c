2db9646759a6d7d95951e6328bfa50f1c800b9b0
dongs <dongstwo@gmail.com>
2023-10-13 23:17:02 +0900

    4321

--- gope 1/FileName.c
+++ gope 1/FileName.c
@@ -4,8 +4,18 @@
 #include <time.h>
 
 int main(void) {
-	jjuggumi_init();
-	sample();
+	int ii = 3;
+	if (ii > 0) {
+		for (int i = 4; i > 0; i--) {
+			printf("%d 곧 게임이 시작됩니다.\n", i);
+			Sleep(1000);
+			system("cls");
+			ii--;
+		}
+			
+	}
+	//jjuggumi_init();
+	//sample();
 	//dialog();
 	//mugunghwa();
 	//nightgame();
